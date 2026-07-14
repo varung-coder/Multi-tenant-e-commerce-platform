1eac67e828a92cef997d3700934eb0371ab94cf5
Tanisha-soam <riyasoam0@gmail.com>
2026-07-14 22:14:07 +0530

    Initial frontend setup with React and basic homepage

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>ShopEase</h2>
+
+      <div>
+        <a href="#">Home</a>{" | "}
+        <a href="#">Products</a>{" | "}
+        <a href="#">Login</a>{" | "}
+        <a href="#">Register</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>Welcome to Multi-Tenant E-Commerce</h1>
+
+      <p>Shop from multiple stores on one platform.</p>
+
+      <button>Explore Stores</button>
+    </div>
+  );
+}
+
+export default Home;
